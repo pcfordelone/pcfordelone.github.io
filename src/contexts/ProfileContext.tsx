f66f8c1e814dd1f39f02c5545952204d2d68f22f
Paulo Cesar Fordelone <pc@fordelone.com.br>
2022-07-19 17:55:40 -0300

    general updates

--- src/contexts/ProfileContext.tsx
+++ src/contexts/ProfileContext.tsx
@@ -1,31 +1,31 @@
-import { ApolloError } from '@apollo/client'
-import { createContext } from 'react'
-import { Profile, useListProfileWithProjectsQuery } from '../graphql/generated'
+import { ApolloError } from "@apollo/client";
+import { createContext } from "react";
+import { Profile, useListProfileWithProjectsQuery } from "../graphql/generated";
 
 interface IProfileContext {
-  isLoading: boolean
-  profile: Profile | undefined
-  error: ApolloError | undefined
+  isLoading: boolean;
+  profile: Profile | undefined;
+  error: ApolloError | undefined;
 }
 
 interface IProfileProviderProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export const ProfileContext = createContext<IProfileContext>({
   isLoading: true,
   profile: undefined,
   error: undefined,
-})
+});
 
 export const ProfileProvider: React.FC<IProfileProviderProps> = ({
   children,
 }) => {
   const { loading, error, data } = useListProfileWithProjectsQuery({
     variables: {
-      id: 'cl4urdvh6uicg0dkdc51du7ld',
+      id: "cl4urdvh6uicg0dkdc51du7ld",
     },
-  })
+  });
 
   return (
     <ProfileContext.Provider
@@ -33,5 +33,5 @@ export const ProfileProvider: React.FC<IProfileProviderProps> = ({
     >
       {children}
     </ProfileContext.Provider>
-  )
-}
+  );
+};
--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,27 +1,28 @@
-import { Cover } from '../components/Cover'
-import { Experience } from '../components/Experience'
-import { Profile } from '../components/Profile/index'
-import { Projects } from '../components/Projects'
-import { Skills } from '../components/Skills'
-import { Education } from '../components/Education/index'
-import { useListProfileWithProjectsQuery } from '../graphql/generated'
-import { Contact } from '../components/Contact'
-import { useContext, useRef } from 'react'
-import { ProfileInfo } from '../components/ProfileInfo'
-import { ProfileContext, ProfileProvider } from '../contexts/ProfileContext'
-import { Loading } from '../components/Loading'
+import { Cover } from "../components/Cover";
+import { Experience } from "../components/Experience";
+import { Profile } from "../components/Profile/index";
+import { Projects } from "../components/Projects";
+import { Skills } from "../components/Skills";
+import { Education } from "../components/Education/index";
+import { useListProfileWithProjectsQuery } from "../graphql/generated";
+import { Contact } from "../components/Contact";
+import { useRef } from "react";
+import { ProfileInfo } from "../components/ProfileInfo";
+
+import { Loading } from "../components/Loading";
+import { useProfile } from "../contexts/useProfile";
 
 export const ProfilePage: React.FC = () => {
-  const profileRef = useRef<HTMLDivElement>(null)
+  const profileRef = useRef<HTMLDivElement>(null);
 
   function handleScrollToProfile() {
-    profileRef.current?.scrollIntoView({ behavior: 'smooth' })
+    profileRef.current?.scrollIntoView({ behavior: "smooth" });
   }
 
-  const { profile, isLoading, error } = useContext(ProfileContext)
+  const { profile, isLoading, error } = useProfile();
 
-  if (isLoading) return <Loading />
-  if (error) return <p>Error...</p>
+  if (isLoading) return <Loading />;
+  if (error) return <p>Error...</p>;
 
   return (
     <>
@@ -34,5 +35,5 @@ export const ProfilePage: React.FC = () => {
       <Education />
       <Contact />
     </>
-  )
-}
+  );
+};
